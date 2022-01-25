85bc344a9ef4f5f7f097b96605be90b4dac2afa1
Vladimir Shkodin <v.s.shkodin@gmail.com>
2022-01-25 09:43:04 -0800

    api tests added

--- src/SimpleTestAPI.java
+++ src/SimpleTestAPI.java
@@ -1,30 +1,45 @@
-package src.SimpleTestUI;
-import src.pages.ApplyPage.ApplyPage;
-import src.pages.LoginPage.LoginPage;
+package src.SimpleTestAPI;
+//import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.*;
 import org.testng.annotations.*;
 import org.testng.Assert;
 
-public class SimpleTestUI {
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.hamcrest.core.CombinableMatcher;
+
+public class SimpleTestAPI {
+	 String payload = "{\"loanAppUuid\":\"b8096ec7-2150-405f-84f5-ae99864b3e96\",\"skipSideEffects\": \"true\" }";
+
 	 @Test()
-	 public void aFastTest() {
-            ApplyPage apply = new ApplyPage();
-            apply.openPage();
-            apply.validatePageTitle();
-            apply.validatePageUrl();
-            apply.applyUser();
-            String[] arr1 = apply.rememberRates();
-            apply.logout();
-            apply.exit();
+	 public void aFastTestOne() {
 
-           LoginPage login = new LoginPage();
-             login.openPage();
-             login.validatePageTitle();
-             login.validatePageUrl();
-             login.authUser();
-             String[] arr2 = login.rememberRates();
-             login.logout();
-             login.exit();
+	 //RestAssured.baseURI ="https://credapi.credify.tech";
+            //given().when().get("http://www.google.com").then().statusCode(200);
+            given()
+                .header("x-cf-source-id", "coding-challenge")
+                .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
+                .header("Content-Type", "application/json")
+                    .body(payload)
+                     .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
+                         .then()
+                          .statusCode(200)
+                          .body(
+                                "loanAppUuid", Matchers.notNullValue()
+            );
+      }
 
-            Assert.assertEquals(arr1, arr2);
-            }
+      	 @Test()
+	 public void aFastTestTwo() {
+	 //RestAssured.baseURI ="https://credapi.credify.tech";
+            //given().when().get("http://www.google.com").then().statusCode(200);
+            given()
+                .header("x-cf-source-id", "coding-challenge")
+                .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
+                .header("Content-Type:", "application/json")
+                    .body(payload)
+                     .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
+                         .then()
+                          .statusCode(500);
+      }
 }
