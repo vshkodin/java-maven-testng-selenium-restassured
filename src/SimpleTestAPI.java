ee1b2e1c029eb85c4c922209d93c7007f9ac2f66
Vladimir Shkodin <v.s.shkodin@gmail.com>
2022-01-25 11:12:18 -0800

    API tests added

--- src/SimpleTestAPI.java
+++ src/SimpleTestAPI.java
@@ -1,45 +1,72 @@
 package src.SimpleTestAPI;
+
+import java.util.*;
 //import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.*;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.parsing.Parser;
+import io.restassured.response.Response;
+import org.json.simple.JSONObject;
+import static io.restassured.RestAssured.given;
 import org.testng.annotations.*;
 import org.testng.Assert;
-
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.hamcrest.core.CombinableMatcher;
 
 public class SimpleTestAPI {
+
 	 String payload = "{\"loanAppUuid\":\"b8096ec7-2150-405f-84f5-ae99864b3e96\",\"skipSideEffects\": \"true\" }";
+     String payloadTwo = "{\"loanAppUuid\":\"b8096ec7-2150-405f-84f5-ae99864b3e97\",\"skipSideEffects\": \"true\" }";
 
-	 @Test()
-	 public void aFastTestOne() {
-
-	 //RestAssured.baseURI ="https://credapi.credify.tech";
-            //given().when().get("http://www.google.com").then().statusCode(200);
+	 public static Response doPostRequest() {
+        RestAssured.defaultParser = Parser.JSON;
+        String payload = "{\"loanAppUuid\":\"b8096ec7-2150-405f-84f5-ae99864b3e96\",\"skipSideEffects\": \"true\" }";
+        return
             given()
                 .header("x-cf-source-id", "coding-challenge")
                 .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
                 .header("Content-Type", "application/json")
-                    .body(payload)
+                .body(payload)
                      .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
                          .then()
                           .statusCode(200)
-                          .body(
-                                "loanAppUuid", Matchers.notNullValue()
-            );
-      }
+                          .contentType(ContentType.JSON).extract().response();
+    }
 
-      	 @Test()
+	 @Test()
+	 public void aFastTestOne() {
+        Response response = doPostRequest();
+        Map<String, String>  usernames = response.jsonPath().getMap("loanAppResumptionInfo");
+         Assert.assertEquals(usernames.get("productType"), "PERSONAL_LOAN");
+         }
+
+
+
+    @Test()
+
 	 public void aFastTestTwo() {
-	 //RestAssured.baseURI ="https://credapi.credify.tech";
-            //given().when().get("http://www.google.com").then().statusCode(200);
             given()
                 .header("x-cf-source-id", "coding-challenge")
                 .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
-                .header("Content-Type:", "application/json")
+                .header("Content-Type:", "application/json")  // Placed error here Content-Type:
                     .body(payload)
                      .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
                          .then()
                           .statusCode(500);
+      }
+
+
+
+     @Test()
+	 public void aFastTestThree() {
+            given()
+                .header("x-cf-source-id", "coding-challenge")
+                .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
+                .header("Content-Type", "application/json")
+                .body(payloadTwo)
+                     .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
+                         .then()
+                          .statusCode(404);
       }
 }
