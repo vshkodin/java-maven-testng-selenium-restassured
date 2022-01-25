f62cbc1f31597580b5953eb3b2d66b6b0231b480
Vladimir Shkodin <v.s.shkodin@gmail.com>
2022-01-25 12:03:09 -0800

    final change

--- readme.md
+++ readme.md
@@ -1,5 +1,12 @@
 Java, Maven, TestNG, Selenium, Restassured
 ### How to run
+Please provide path to chromedriver here java-maven-testng-selenium-restassured/src/pages/BaseDriver.java line 36
+```
+            System.setProperty("webdriver.chrome.driver", "C:/tools/chromedriver.exe");
+```
+
+https://github.com/vsshk/java-maven-testng-selenium-restassured/blob/main/src/pages/BaseDriver.java#L36
+# Run
 ```
 $ mvn  test
 ```
--- src/SimpleTestAPI.java
+++ src/SimpleTestAPI.java
@@ -1,7 +1,6 @@
 package src.SimpleTestAPI;
 
 import java.util.*;
-//import static io.restassured.RestAssured.given;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.parsing.Parser;
@@ -18,54 +17,38 @@ public class SimpleTestAPI {
 
 	 String payloadTrue = "{\"loanAppUuid\":\"b8096ec7-2150-405f-84f5-ae99864b3e96\",\"skipSideEffects\": \"true\" }";
      String payloadFalse = "{\"loanAppUuid\":\"b8096ec7-2150-405f-84f5-ae99864b3e97\",\"skipSideEffects\": \"true\" }";
+     String baseUrl = "https://credapi.credify.tech";
+     String endPoint = baseUrl + "/api/brfunnelorch/v2/resume/byLeadSecret";
 
-	 public Response doPostRequest() {
+	 public Response doPostRequest(String payload, int statusCode ) {
         RestAssured.defaultParser = Parser.JSON;
         return
             given()
                 .header("x-cf-source-id", "coding-challenge")
                 .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
                 .header("Content-Type", "application/json")
-                .body(payloadTrue)
-                     .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
+                .body(payload)
+                     .when().post(endPoint)
                          .then()
-                          .statusCode(200)
+                          .statusCode(statusCode)
                           .contentType(ContentType.JSON).extract().response();
     }
 
 	 @Test()
-	 public void aFastTestOne() {
-     Response response = doPostRequest();
-     Map<String, String>  usernames = response.jsonPath().getMap("loanAppResumptionInfo");
-     Assert.assertEquals(usernames.get("productType"), "PERSONAL_LOAN");
+	 public void apiTestloanAppUuidProvided() {
+         Response response = doPostRequest(payloadTrue,200);
+         Map<String, String>  usernames = response.jsonPath().getMap("loanAppResumptionInfo");
+         Assert.assertEquals(usernames.get("productType"), "PERSONAL_LOAN");
       }
-
-
-
-    @Test()
-
-	 public void aFastTestTwo() {
-            given()
-                .header("x-cf-source-id", "coding-challenge")
-                .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
-                .header("Content-Type:", "application/json")  // Placed error here Content-Type:
-                    .body(payloadTrue)
-                     .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
-                         .then()
-                          .statusCode(500);
-      }
-
 
 
      @Test()
-	 public void aFastTestThree() {
-            given()
-                .header("x-cf-source-id", "coding-challenge")
-                .header("x-cf-corr-id", "31b98526-7e02-11ec-90d6-0242ac120003")
-                .header("Content-Type", "application/json")
-                .body(payloadFalse)
-                     .when().post("https://credapi.credify.tech/api/brfunnelorch/v2/resume/byLeadSecret")
-                         .then()
-                          .statusCode(404);
+	 public void apiTestLoanAppUuidDoesNotExist() {
+            Response response = doPostRequest(payloadFalse,404);
+      }
+
+     @Test()
+	 public void apiTestNegativeScenario() {
+            given().header("LOL","Lol").body(payloadTrue).when().post(endPoint).then().statusCode(500);
       }
 }
